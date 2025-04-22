491b5ca3f8fac68917089480fa2fd46329719c86
HQ\abe <abinesh.e@kudelskisecurity.com>
2025-04-22 06:13:46 +0530

    update repeatable

---
+++ repeatable/6-recursion.js
@@ -0,0 +1,73 @@
+// 📘 1. Reverse a String
+function reverseString(str) {
+
+}
+
+console.log(reverseString('hello')); // "olleh"
+//
+//
+//
+//
+//
+
+// 🔍 2. Palindrome Check (Recursively)
+function isPalindrome() {
+
+}
+
+console.log(isPalindrome('racecar')); // true
+console.log(isPalindrome('hello')); // false
+//
+//
+//
+//
+//
+
+// 🧮 3. Sum of Digits OR Factorial
+function factorial() {
+
+}
+
+console.log(factorial(5)); // 120
+//
+//
+//
+//
+//
+
+//🔢 Sum of Digits
+function sumOfDigits() {
+
+}
+
+console.log(sumOfDigits(1234)); // 10
+//
+//
+//
+//
+//
+
+//🌿 4. Power Set (Subsets)
+function powerSet() {
+ 
+}
+
+console.log(powerSet([1, 2, 3]));
+//
+//
+//
+//
+//
+
+// 🔄 5. Permutations
+function permute() {
+
+}
+
+console.log(permute([1, 2, 3]));
+/*
+  [
+    [1,2,3], [1,3,2], [2,1,3],
+    [2,3,1], [3,1,2], [3,2,1]
+  ]
+  */
